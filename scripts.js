d946b384b9c43309defd2b56dd1838ef71f50233
Blake Chartrand <chartrandblake@hotmail.com>
2020-12-30 21:30:24 -0800

    Styled app to look better

--- scripts.js
+++ scripts.js
@@ -1,93 +1,83 @@
 //Sets the important variables
-
 const container = document.getElementById('container');
-
 let rows = document.getElementsByClassName('gridRow');
 let cells = document.getElementsByClassName('cell');
 
 //Function for making the grid
-
 function makeGrid(a) {
-    getRidGrid();
-    makeRows(a);
-    makeColumns(a);
+	getRidGrid();
+	makeRows(a);
+	makeColumns(a);
 }
 
-function removeGrid() {
-    
-}
-
+function removeGrid() {}
 makeGrid(16);
 draw();
 
-
 //Makes rows
 function makeRows(num) {
-    for (r = 0; r<num; r++ ) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow"
-    }
+	for (r = 0; r < num; r++) {
+		let row = document.createElement("div");
+		container.appendChild(row).className = "gridRow"
+	}
 }
-
 //Makes Columns
 function makeColumns(num) {
-    for (c = 0; c < num; c++) {
-        for (j = 0; j < num; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        }
-    }
+	for (c = 0; c < num; c++) {
+		for (j = 0; j < num; j++) {
+			let newCell = document.createElement("div");
+			rows[j].appendChild(newCell).className = "cell";
+		}
+	}
 }
 
-
 function erase() {
-    let eraser = document.querySelectorAll(".cell");
-    const resetBackgroundColor = e => {
-        e.target.style.backgroundColor = "white";
-    }
-    eraser.forEach(div => {
-        div.addEventListener("mouseover", resetBackgroundColor, {once: true})
-    })
-        }
-
+	let eraser = document.querySelectorAll(".cell");
+	const resetBackgroundColor = e => {
+		e.target.style.backgroundColor = "white";
+	}
+	eraser.forEach(div => {
+		div.addEventListener("mouseover", resetBackgroundColor, {
+			once: true
+		})
+	})
+}
 
 function draw() {
-let divs = document.querySelectorAll(".cell");
-const changeBackgroundColor = e => {
- e.target.style.backgroundColor = "red";
- }
-divs.forEach(div => {
- div.addEventListener("mouseover", changeBackgroundColor, {once: true});
-})}
-
+	let divs = document.querySelectorAll(".cell");
+	const changeBackgroundColor = e => {
+		e.target.style.backgroundColor = "teal";
+	}
+	divs.forEach(div => {
+		div.addEventListener("mouseover", changeBackgroundColor, {
+			once: true
+		});
+	})
+}
 
 function reset() {
-    const gridItems = document.querySelectorAll('.cell');
-  
-    gridItems.forEach((item) => {
-      item.style.backgroundColor = '#FFF';
-    });
-
-    draw();
-  }
+	const gridItems = document.querySelectorAll('.cell');
+	gridItems.forEach((item) => {
+		item.style.backgroundColor = 'white';
+	});
+	draw();
+}
 
 function getRidGrid() {
+	var myNode = document.getElementById("container");
+	var fc = myNode.firstChild;
+	while (fc) {
+		myNode.removeChild(fc);
+		fc = myNode.firstChild;
+	}
+}
 
-    var myNode = document.getElementById("container");
-    var fc = myNode.firstChild;
-    
-    while( fc ) {
-        myNode.removeChild( fc );
-        fc = myNode.firstChild;
-    }}
-
-    function changeSize() {
-        let newSize = prompt("Enter a new size");
-
-        if (newSize !== null) {
-            if (newSize < 65) {
-            newSize = parseInt(newSize);
-            makeGrid(newSize);
-        }
-        else alert('Sorry, please use a smaller number.')}}
-    
+function changeSize() {
+	let newSize = prompt("Enter a new size");
+	if (newSize !== null) {
+		if (newSize < 65) {
+			newSize = parseInt(newSize);
+			makeGrid(newSize);
+		} else alert('Sorry, please use a smaller number.')
+	}
+}
